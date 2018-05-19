48c3bbda6af9f0c4d276d388a930ecab7a650257
Santosh Kumar <sntshkmr60@gmail.com>
2018-05-19 21:17:02 +0530

    pymel implementation

--- playground/objectSpreader.py
+++ playground/objectSpreader.py
@@ -1,15 +1,20 @@
 import random
 import pymel.core as pm
 
-spreaderWindow = pm.window(title="Spreader", wh=(300, 200))
+if pm.window(spreaderWindow, exists=True):
+    pm.deleteUI(spreaderWindow)
+
+spreaderWindow = pm.window(title="Spreader", wh=(200, 100))
 layout = pm.columnLayout()
 pm.button(label="Spread Selected", command="spread()")
 pm.showWindow(spreaderWindow)
 
+
 def spread():
     selList = pm.ls(sl=True)
     for obj in selList:
-        rangeX = random.randint(-10, 10)
-        rangeZ = random.randint(-10, 10)
-        pm.setAttr(obj + ".translateX", rangeX)
-        pm.setAttr(obj + ".translateZ", rangeZ)
+        rangeX = random.uniform(-10.0, 10.0)
+        rangeZ = random.uniform(-10.0, 10.0)
+        obj.setAttr('translateX', rangeX)
+        obj.setAttr('translateZ', rangeZ)
+
