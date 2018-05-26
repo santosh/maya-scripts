5e224c74c2f9acd729cd8e283ffea3bc653e539c
Santosh Kumar <sntshkmr60@gmail.com>
2018-05-26 08:24:56 +0530

    refactor

--- sk_utils.py
+++ sk_utils.py
@@ -1,37 +1,38 @@
+from __future__ import absolute_import, unicode_literals, print_function
 import os
+import pymel.core as pm
 import maya.cmds as cmds
-
-def createSphereSpiral(no_of_spheres):
-    for i in range(no_of_spheres):
-        sphere = cmds.sphere(name="SpiralSphere#", pivot=[3, i*(0.5), 0])
-        cmds.setAttr(sphere[0] + ".rotateY", i*30)
-
 
 
 def texturePathFixer():
-    print "Info: set project before executing"
+    # TODO: Make it pymel (OO, replace setAttr with set)
+    print("Info: set project before executing")
 
-    fileNodes = cmds.ls(type="file", long=True)
+    file_nodes = cmds.ls(type="file", long=True)
 
-    for f in fileNodes:
-        oldTexturePath = cmds.getAttr(f + ".fileTextureName")
+    for f in file_nodes:
+        old_texture_path = cmds.getAttr(f + ".fileTextureName")
         # use os module for compat, and not string.split()
-        newTexturePath = os.path.join("textures", os.path.split(oldTexturePath)[-1])
-        print "Old:", oldTexturePath
-        print "New:", newTexturePath
-        cmds.setAttr(f + ".fileTextureName", newTexturePath, type="string")
+        new_texture_path = os.path.join("textures", os.path.split(old_texture_path)[-1])
+        print("Old:", old_texture_path)
+        print("New:", new_texture_path)
+        cmds.setAttr(f + ".fileTextureName", new_texture_path, type="string")
 
 
 def gainLights(gain):
     """
     Changes the intensity of the available lights at once.
+
+    Multiplies the value of gain, into the intensity of every
+    light in the scene.
     """
     # select all lights
-    lights = cmds.ls(lights=True)
-    cmds.select(lights)
 
-    for light in lightsList:
-        oldInten = cmds.getAttr(light + ".intensity")
-        newInten = oldInten * gain
-        cmds.setAttr(light + ".intensity", newInten)
-        print light, "Old: ", oldInten, "New: ", newInten
+    lights = pm.ls(lights=True)
+    pm.select(lights)
+
+    for light in lights:
+        old_inten = light.get("intensity")
+        new_inten = old_inten * gain
+        cmds.setAttr(light + ".intensity", new_inten)
+        print(light, "Old: ", old_inten, "New: ", new_inten)
