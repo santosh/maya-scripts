13855384e44ee386d10bdfccc29c011a503bc80e
Santosh Kumar <sntshkmr60@gmail.com>
2018-05-29 07:22:11 +0530

    refactored

--- mayasphere.py
+++ mayasphere.py
@@ -9,32 +9,43 @@ class MayaSphere(MayaGeom):
         kwargs['name'] = name
         kwargs['object'] = True
 
-        parts = pm.sphere(**kwargs)
+        self._create(kwargs)
+
+    def __del__(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+    def _create(self, opts={}):
+        parts = pm.sphere(**opts)
         self.name = parts[0]
 
+    def delete(self):
+        if self.exists():
+            pm.delete(self.name)
+
+    def exists(self):
+        return pm.objExists(self.name)
+
     def setTranslation(self, x=None, y=None, z=None):
-        for name in ('x', 'y', 'z'):
-            val = locals()[name]
-            if val is not None:
-                opts = {name: True, 'objectSpace': True, 'absolute': True}
-                pm.move(val, self.name, **opts)
+        self._doTransform(pm.move, x, y, z)
 
     def getRotation(self):
         return pm.xform(self.name, q=1, ro=1)  # ro=rotation
 
     def setRotation(self, x=None, y=None, z=None):
-        for name in ('x', 'y', 'z'):
-            val = locals()[name]
-            if val is not None:
-                opts = {name: True, 'objectSpace': True, 'absolute': True}
-                pm.rotate(val, self.name, **opts)
+        self._doTransform(pm.rotate, x, y, x)
 
     def getScale(self):
         return pm.xform(self.name, q=1, scale=1)
 
     def setScale(self, x=None, y=None, z=None):
+        self._doTransform(pm.scale, x, y, z)
+
+    def _doTransform(self, func, x, y, z):
         for name in ('x', 'y', 'z'):
             val = locals()[name]
             if val is not None:
                 opts = {name: True, 'objectSpace': True, 'absolute': True}
-                pm.scale(val, self.name, **opts)
+                func(val, self.name, **opts)
